d67351c8d2c362469d28507729af3e514fe27762
oskarrrrrrr <o.tolkacz@gmail.com>
2019-06-17 20:38:57 +0200

    Complete initial database setup.
    Needs some more work but all function are already there and work more or
    less.

--- sql/init.sql
+++ sql/init.sql
@@ -7,6 +7,33 @@ CREATE EXTENSION pgcrypto;
 CREATE TYPE action_t AS ENUM ('support', 'protest');
 CREATE TYPE vote_t AS ENUM ('upvote', 'downvote');
 
+CREATE TYPE action_ret AS(
+    action_id integer,
+    action_type action_t,
+    project_id integer,
+    authority_id integer,
+    upvotes integer,
+    downvotes integer
+);
+
+CREATE TYPE project_ret AS(
+    project_id integer,
+    authority_id integer
+);
+
+CREATE TYPE votes_ret AS(
+    member_id integer,
+    upvotes integer,
+    downvotes integer
+);
+
+CREATE TYPE trolls_ret AS(
+    member_id integer,
+    upvotes integer,
+    downvotes integer,
+    active boolean
+);
+
 ------------
 -- TABLES --
 ------------
@@ -104,7 +131,7 @@ CREATE OR REPLACE FUNCTION handle_member(member_id integer, password text, times
         password_ok boolean;
     BEGIN
 
-        IF member_is_frozen(member_id, password) THEN
+        IF is_member_frozen(member_id, timestmp) THEN
             RETURN False;
         END IF;
 
@@ -117,7 +144,6 @@ CREATE OR REPLACE FUNCTION handle_member(member_id integer, password text, times
         END IF;
         RETURN True;
     END;
- 
 $$ LANGUAGE plpgsql;
 
 
@@ -128,22 +154,18 @@ CREATE OR REPLACE FUNCTION check_password(member_id integer, password text) RETU
 $$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION add_project(project_id integer, authority_id integer) RETURNS void AS $$
+CREATE OR REPLACE FUNCTION add_project(project_id integer, authority_id integer) RETURNS boolean AS $$
     BEGIN
         INSERT INTO unique_ids VALUES(project_id), (authority_id);
-        INSERT INTO project VALUES(project_id, authorit_id);
+        INSERT INTO authority VALUES(authority_id);
+        INSERT INTO project VALUES(project_id, authority_id);
+        RETURN True;
     END
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE FUNCTION add_action(action_id integer ) RETURNS void AS $$
-    BEGIN
-        
-    END
-$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION add_action(
-    action_type text,
+    action_type action_t,
     timestmp bigint,
     member_id integer,
     password text,
@@ -154,7 +176,6 @@ CREATE OR REPLACE FUNCTION add_action(
     DECLARE
         member_ok boolean;
         project_ok boolean;
-        action_ok boolean;
     BEGIN 
 
         SELECT handle_member(member_id, password, timestmp) INTO member_ok;
@@ -169,23 +190,22 @@ CREATE OR REPLACE FUNCTION add_action(
             END IF;
         END IF;
     
-        SELECT add_action(action_id) INTO action_ok;
-        IF NOT action_ok THEN
-            RETURN False;
-        END IF;
+        INSERT INTO unique_ids VALUES (action_id); 
+        INSERT INTO action VALUES (action_id, project_id, member_id, action_type);
 
         RETURN True;
         
     END;
-$$ LANGUAGE plpgsql security definer;
+$$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION vote(
+CREATE OR REPLACE FUNCTION add_vote(
     vote_type vote_t,
     member_id integer,
     action_id integer
-    ) RETURNS void AS $$
+    ) RETURNS boolean AS $$
     BEGIN
-        
+        INSERT INTO vote VALUES(action_id, member_id, vote_type);
+        RETURN True; 
     END;
 $$ LANGUAGE plpgsql security definer;
 
@@ -196,10 +216,9 @@ $$ LANGUAGE plpgsql security definer;
 
 CREATE OR REPLACE FUNCTION leader(timestmp bigint, password text, member_id integer) RETURNS void AS $$
     BEGIN
-        SELECT add_member(member_id, text, bigint, True);
+        PERFORM add_member(member_id, password, timestmp, True);
     END;
 $$ LANGUAGE plpgsql security definer;
-
 
 CREATE OR REPLACE FUNCTION support(
     timestmp bigint,
@@ -208,12 +227,11 @@ CREATE OR REPLACE FUNCTION support(
     action_id integer,
     project_id integer,
     authority_id integer DEFAULT NULL
-    ) RETURNS integer AS $$
+    ) RETURNS boolean AS $$
     BEGIN
         RETURN add_action('support', timestmp, member_id, password, action_id, project_id, authority_id);
     END;
 $$ LANGUAGE plpgsql;
-
 
 CREATE OR REPLACE FUNCTION protest(
     timestmp bigint,
@@ -222,7 +240,7 @@ CREATE OR REPLACE FUNCTION protest(
     action_id integer,
     project_id integer,
     authority_id integer DEFAULT NULL
-    ) RETURNS integer AS $$
+    ) RETURNS boolean AS $$
     BEGIN
         RETURN add_action('protest', timestmp, member_id, password, action_id, project_id, authority_id);
     END;
@@ -233,9 +251,20 @@ CREATE OR REPLACE FUNCTION upvote(
     member_id integer,
     password text,
     action_id integer
-    ) RETURNS void AS $$
+    ) RETURNS boolean AS $$
+    DECLARE
+        member_ok boolean;
     BEGIN
-        RETURN vote('upvote', member_id, action_id);        
+        SELECT handle_member(member_id, password, timestmp) INTO member_ok;
+        IF NOT member_ok THEN 
+            RETURN False;
+        END IF;
+        
+        IF action_id NOT IN (SELECT id FROM action) THEN
+            RETURN False;
+        END IF;
+
+        RETURN add_vote('upvote', member_id, action_id);
     END;
 $$ LANGUAGE plpgsql security definer;
 
@@ -244,9 +273,93 @@ CREATE OR REPLACE FUNCTION downvote(
     member_id integer,
     password text,
     action_id integer
-    ) RETURNS void AS $$
+    ) RETURNS boolean AS $$
+    DECLARE
+        member_ok boolean;
     BEGIN
-        RETURN vote('downvote', member_id, action_id);  
+        SELECT handle_member(member_id, password, timestmp) INTO member_ok;
+        IF NOT member_ok THEN 
+            RETURN False;
+        END IF;
+
+        IF action_id NOT IN (SELECT id FROM action) THEN
+            RETURN False;
+        END IF;
+
+        RETURN add_vote('downvote', member_id, action_id);  
+    END;
+$$ LANGUAGE plpgsql security definer;
+
+CREATE OR REPLACE FUNCTION actions(
+    timestmp bigint,
+    member_id integer,
+    password text,
+    action_type action_t DEFAULT NULL,
+    project_id integer DEFAULT NULL,
+    authority_id integer DEFAULT NULL
+    ) RETURNS action_ret AS $$
+    DECLARE
+        ret action_ret;
+    BEGIN
+
+        IF is_member_frozen(member_id, timestmp) THEN
+            --OR (SELECT * FROM member WHERE member.id = member_id AND member.is_leader) IS NULL THEN
+            RETURN False;
+        END IF;
+        
+        SELECT action.id, action.action_type, action.project_id, project.authority_id, SUM(member.upvotes)::int, SUM(member.downvotes)::int
+        FROM action JOIN member ON action.member_id = member.id
+            JOIN project ON action.project_id = project.id
+        GROUP BY action.id, project.authority_id
+        ORDER BY action.id ASC INTO ret;
+
+        -- TODO filter using optional parameters
+        
+        RETURN ret;
+
+    END;
+$$ LANGUAGE plpgsql security definer;
+
+CREATE OR REPLACE FUNCTION projects(
+    timestmp bigint,
+    member_id integer,
+    password text,
+    authority_id integer DEFAULT NULL
+    ) RETURNS project_ret AS $$
+    DECLARE
+        ret project_ret;
+    BEGIN
+        SELECT * FROM project INTO ret;
+        RETURN ret; 
+    END;
+$$ LANGUAGE plpgsql security definer;
+
+CREATE OR REPLACE FUNCTION votes(
+    timestmp bigint,
+    member_id integer,
+    password text,
+    action_id integer DEFAULT NULL,
+    project_id integer DEFAULT NULL
+    ) RETURNS SETOF votes_ret AS $$
+    BEGIN
+        RETURN QUERY
+            SELECT member.id,
+                (COUNT(vote.action_id) FILTER (WHERE vote.vote_type = 'upvote'))::int AS upvotes,
+                (COUNT(vote.action_id) FILTER (WHERE vote.vote_type = 'downvote'))::int AS downvotes
+            FROM member LEFT JOIN vote ON member.id = vote.member_id
+            GROUP BY member.id
+            ORDER BY member.id ASC;
+    END;
+$$ LANGUAGE plpgsql security definer;
+
+CREATE OR REPLACE FUNCTION trolls(
+    timestmp bigint
+    ) RETURNS SETOF trolls_ret AS $$
+    BEGIN
+        RETURN QUERY
+            SELECT member.id, member.upvotes, member.downvotes, is_member_frozen(member.id, timestmp)
+            FROM member
+            ORDER BY member.downvotes - member.upvotes;
     END;
 $$ LANGUAGE plpgsql security definer;
 
@@ -262,3 +375,7 @@ GRANT EXECUTE ON FUNCTION support  TO app;
 GRANT EXECUTE ON FUNCTION protest  TO app;
 GRANT EXECUTE ON FUNCTION upvote   TO app;
 GRANT EXECUTE ON FUNCTION downvote TO app;
+GRANT EXECUTE ON FUNCTION actions  TO app;
+GRANT EXECUTE ON FUNCTION projects TO app;
+GRANT EXECUTE ON FUNCTION votes    TO app;
+GRANT EXECUTE ON FUNCTION trolls   TO app;
---
+++ sql/test_in.sql
@@ -0,0 +1,8 @@
+select * from support(1, 1, 'rty', 2, 3, 4);
+select * from upvote(2, 5, 'chuj', 2);
+select * from downvote(3, 1, 'rty', 2);
+select * from leader(4, 'qwe', 6);
+select * from actions(7, 6, 'qwe');
+select * from projects(8, 1, 'rty');
+select votes(10, 1, 'qwe');
+select trolls(20);
